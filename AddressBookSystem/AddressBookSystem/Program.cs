c6a7085511317801021cfd956bb14e96f5cb4d24
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-05 16:42:31 +0530

    [Kumar Shubham]Refactor

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -8,12 +8,12 @@ namespace AddressBookSystem
         {
             Console.WriteLine("*****Welcome to Address Book System*****");
             ///putting value in constructor
-            Contacts contacts = new Contacts("Kumar", "Shubham", "Bihar", "Bihar Sharif", "Nai Sarai", "803101", "1032602310", "kmrshubham123@gmail.com");
+            Contacts contacts = new Contacts("Kumar", "Shubham", "Bihar", "Bihar Sharif", "Nai Sarai", 803101, 1032602310, "kmrshubham123@gmail.com");
             Console.WriteLine("First Name:" + contacts.FirstName);
             Console.WriteLine("Last Name:" + contacts.LastName);
             Console.WriteLine("State Name:" + contacts.State);
             Console.WriteLine("City Name:" + contacts.City);
-            Console.WriteLine("Address Name:" + contacts.City);
+            Console.WriteLine("Address Name:" + contacts.Address);
             Console.WriteLine("Phone No:" + contacts.PhoneNumber);
             Console.WriteLine("Zip Code:" + contacts.ZipCode);
             Console.WriteLine("Email Address:" + contacts.Email);
