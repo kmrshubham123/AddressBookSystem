7de3b2e5b1637cbe2c18ac42b60872ca442266a1
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-08 14:23:42 +0530

    [Kumar Shubham]Added:Add a new contact in address book through console

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -7,9 +7,24 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to Address Book System*****");
-          
             AddressBook addressBook = new AddressBook();
-            addressBook.AddContact("Kumar", "Shubham", "Nai sarai", "Bihar Sharif", "Bihar",  803101, 5698531895,"Kmrshubham123@gmail.com");
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            int zipCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            addressBook.AddContact(firstName, lastName, address, city, state, zipCode,  phoneNumber, email);
             addressBook.ViewContact();
         }
     }
