fb2a3676c0b50fbbdda3e7bcebdb9c6a8b95a4be
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-22 18:29:45 +0530

    [Kumar Shubham]Refactor:In Icontacts

--- AddressBookSystem/AddressBookSystem/IContacts.cs
+++ AddressBookSystem/AddressBookSystem/IContacts.cs
@@ -6,13 +6,10 @@ namespace AddressBookSystem
 {
     interface IContacts
     {
-        interface IContacts
-        {
-            public void AddContact(string firstName, string lastName, string address, string city, string state,  int zipCode, long phoneNumber, string email);
-            public void EditContact(string name);
-            public void DeleteContact(string name);
-            public void ViewContact(string name);
-        }
+        public void AddContact(string firstName, string lastName, string address, string city, string state,  int zipCode, long phoneNumber, string email);
+        public void EditContact(string name);
+        public void DeleteContact(string name);
+        public void ViewContact(string name);
     }
     
 }
